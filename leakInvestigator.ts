b225fbd555c6c21f5c1238f0b880dc73a108fb67
Rettar5 <rettar.v@gmail.com>
2018-07-19 09:32:50 +0900

    ログにOSのメモリ使用量まで記載するよう修正

--- leakInvestigator.ts
+++ leakInvestigator.ts
@@ -1,5 +1,7 @@
 import { Log } from "../../../odnUtils";
 import { AccountConfigs, AccountData } from "../../../configs/accountConfigs";
+import { OdnTweets } from "../../../odnTweets";
+declare let os;
 
 export class LeakInvestigator {
   constructor(private accountData: AccountData, private nowDate: Date, private fullName: string) {}
@@ -11,8 +13,18 @@ export class LeakInvestigator {
    */
   run(finish: (isProcessed?: boolean) => void) {
     const heapUsed = process.memoryUsage().heapUsed;
-    const heapUsedMB = heapUsed / (1024 * 1024);
-    Log.i('LeakInvestigator heap used: ' + heapUsed + 'byte, ' + heapUsedMB.toFixed(1) + 'MB');
+    let text = '@subSC13 ODN-CLIENT サーバステータス\n';
+    text += 'TotalMem: ' + this.convByteToMega(os.totalmem()) + '\n';
+    text += 'FreeMem: ' + this.convByteToMega(os.freemem()) + '\n';
+    text += 'ProcessMem: ' + this.convByteToMega(heapUsed) + '\n';
+    Log.i(text);
+
+    if (this.isValidTweetTime(this.nowDate)) {
+      const t = new OdnTweets(this.accountData);
+      t.text = text;
+      t.postTweet();
+    }
+
     finish();
   }
 
@@ -26,7 +38,18 @@ export class LeakInvestigator {
   static isValid(accountData: AccountData, nowDate: Date): boolean {
     return true; // 毎分実行
   }
+
+  isValidTweetTime(nowDate: Date): boolean {
+    return Constants.tweetHour.some((hour) => {
+      return hour === nowDate.getHours() && 0 === nowDate.getMinutes();
+    });
+  }
+
+  private convByteToMega(byte: number): number {
+    return (byte / (1024 * 1024)).toFixed(1);
+  }
 }
 
 namespace Constants {
+  export const tweetHour = [0, 12];
 }
